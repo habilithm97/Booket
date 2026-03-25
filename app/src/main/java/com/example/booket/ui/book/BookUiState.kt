4a89a0aa71fa0d60b2d97f79e1c55728ab54ca7e
habin <habinkim97@gmail.com>
2026-03-25 23:27:06 +0900

    BookUiState 추가 (UI 상태 관리 sealed class)

---
+++ app/src/main/java/com/example/booket/ui/book/BookUiState.kt
@@ -0,0 +1,11 @@
+package com.example.booket.ui.book
+
+import com.example.booket.data.remote.model.BookResponse
+
+// 화면 상태를 제한된 타입으로 안전하게 표현
+sealed class BookUiState {
+    object Idle : BookUiState() // 초기 상태
+    object Loading : BookUiState() // 데이터 로딩 중 상태
+    data class Success(val data: BookResponse) : BookUiState() // 데이터 요청 성공 및 결과 포함 상태
+    data class Error(val message: String) : BookUiState() // 데이터 요청 실패 및 에러 메시지 포함 상태
+}
---
+++ app/src/main/java/com/example/booket/ui/book/BookViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.booket.ui.book
+
+class BookViewModel {
+}
